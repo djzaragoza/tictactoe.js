2a856d55e69e1c4bdc5860bc79dbeec7e83bb6f0
DJ Zaragoza <zaragoza0126@gmail.com>
2019-10-26 09:13:15 -0700

    first commit

---
+++ checkForWinner.js
@@ -0,0 +1,8 @@
+function checkForWinner () {
+        console.log("checking for winner...")
+        var winner 
+        var playerOne = getPlayerOne() //playerOne is always blue!
+            //console.log ('checkForWinner, playerOne is: ', playerOne)
+
+var computer = (playerOne === "X") ? 
+}
