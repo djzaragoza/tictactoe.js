8b5c4d3525e9294c14f8e43615d5839223b28181
DJ Zaragoza <zaragoza0126@gmail.com>
2019-10-27 17:29:56 -0700

    working on checkforwinner file

--- checkForWinner.js
+++ checkForWinner.js
@@ -4,5 +4,18 @@ function checkForWinner () {
         var playerOne = getPlayerOne() //playerOne is always blue!
             //console.log ('checkForWinner, playerOne is: ', playerOne)
 
-var computer = (playerOne === "X") ? 
+var computer = (playerOne === "X") ? "O" : "X"
+
+// there are 8 winning combos:
+    //three rows, three columns, two diagonals
+    //refactor below .... this is not DRY
+
+    var blueWin1 = $("#one.blue, #two.blue, #three.blue").length === 3
+    var blueWin2 = $("#four.blue, #five.blue, #six.blue").length === 3
+    var blueWin3 = $("#seven.blue, #eight.blue, #nine.blue").length === 3
+    
+    var blueWin4 = $("#one.blue, #four.blue, #seven.blue").length === 3
+    var blueWin5 = $("#two.blue, #five.blue, #eight.blue").length === 3
+    var blueWin6 = $("#three.blue, #six.blue, #nine.blue").length === 3
+    var blueWin7 = $("#one.blue, #five.blue #nine.blue").length === 3
 }
